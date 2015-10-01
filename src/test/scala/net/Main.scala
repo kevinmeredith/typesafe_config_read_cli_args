89a4c62cb766d00b0bb4bf47e88b77b473d87cd5
kevinmeredith <kevin.m.meredith@gmail.com>
2015-09-30 21:54:45 -0400

    Show 'withFallback' example.

--- src/test/scala/net/Main.scala
+++ src/test/scala/net/Main.scala
@@ -4,7 +4,13 @@ import com.typesafe.config.ConfigFactory
 
 object Main extends App {
 	override def main(args: Array[String]) {
-		val x = ConfigFactory.load().getString("app.bar")
-		println(s"x: $x")
+		val baseConfig = ConfigFactory.load()
+		val appConfig = baseConfig.getConfig("app")
+		val hardCodedConfig = baseConfig.getConfig("hard-coded")
+		val merged = appConfig.withFallback(hardCodedConfig)
+
+		val bar   = merged.getString("bar")
+		val bippy = merged.getString("bippy")
+		println(s"bar: $bar | bippy : $bippy")
 	}
 }
